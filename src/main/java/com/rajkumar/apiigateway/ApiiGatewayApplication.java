751e27387b97c4b02f4f4b138de08c98199ba73c
Rajkumar Alagar <967rajkumar@gmail.com>
2020-10-31 13:31:05 +0530

    first commit

---
+++ src/main/java/com/rajkumar/apiigateway/ApiiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.rajkumar.apiigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiiGatewayApplication.class, args);
+	}
+
+}
